59e3ec9894b91ad2d72b5ac5aae92a50e387805a
Zulkar-Jahin <zulkarjahinnizu2000@gmail.com>
2026-05-07 06:42:27 +0600

    fix: counter circle resize

--- src/pages/Counter.jsx
+++ src/pages/Counter.jsx
@@ -2,12 +2,13 @@ import { useState } from "react";
 
 function Counter({ selectedbtn }) {
   const [count, setCount] = useState(0);
+  const [selectedButton, setSelectedNum] = useState(null);
 
   const target = 500;
+  const radius = 118;
   const progress = Math.min((count / target) * 100, 100);
-  const circumference = 2 * Math.PI * 88;
+  const circumference = 2 * Math.PI * radius;
   const offset = circumference - (progress / 100) * circumference;
-
 
   return (
     <div className="bg-[#06110D] min-h-screen text-white p-4 lg:p-6 flex flex-col lg:flex-row gap-6 items-center justify-center">
@@ -45,44 +46,47 @@ function Counter({ selectedbtn }) {
         {/* Circular counter */}
         <div className="relative w-52 h-52">
           <svg
-            className="absolute top-0 left-0 -rotate-90"
-            width="208"
-            height="208"
-            viewBox="0 0 208 208"
+            className="absolute -top-44 -left-5 -rotate-90"
+            width="408"
+            height="408"
+            viewBox="0 0 408 408"
           >
             <circle
-              cx="104"
-              cy="104"
-              r="88"
+              cx="124"
+              cy="124"
+              r="118"
               fill="none"
               stroke="#1a3a2a"
-              strokeWidth="10"
+              strokeWidth="15"
             />
             <circle
-              cx="104"
-              cy="104"
-              r="88"
+              cx="124"
+              cy="124"
+              r = "118"
               fill="none"
               stroke="#1D9E75"
-              strokeWidth="10"
+              strokeWidth="15"
               strokeDasharray={circumference}
               strokeDashoffset={offset}
               strokeLinecap="round"
             />
           </svg>
-          <div className="absolute inset-0 flex items-center justify-center text-6xl font-medium">
+          <div className="absolute inset-0 flex items-center justify-center text-6xl lg:text-8xl font-medium">
             {count}
           </div>
         </div>
 
         {/* Buttons */}
-        <div className="grid grid-cols-4 gap-3 w-full max-w-xs mx-auto">
+        <div className="grid grid-cols-4 gap-3 w-full max-w-xs mx-auto mt-8 lg:mt-20">
           {[1, 10, 50, 100].map((num) => (
             <button
               key={num}
-              onClick={() => setCount(count + num)}
-              className={`py-3 rounded-xl text-base font-medium border transition ${
-                num === 100
+              onClick={() => {
+                setCount(count + num);
+                setSelectedNum(num);
+              }}
+              className={`py-3 rounded-xl text-base  font-medium lg:text-2xl border transition ${
+                num === selectedButton
                   ? "bg-[#0F6E56] text-[#E1F5EE] border-[#085041]"
                   : "bg-[#0f2d1f] text-white border-gray-700 hover:bg-[#1a3a2a]"
               }`}
@@ -94,7 +98,7 @@ function Counter({ selectedbtn }) {
 
         <button
           onClick={() => setCount(0)}
-          className="w-full max-w-sm py-2 rounded-xl border border-gray-700 bg-[#0f2d1f] text-gray-400 hover:text-white transition text-sm"
+          className="w-full max-w-sm py-2 rounded-xl border border-gray-700 bg-[#0f2d1f] text-gray-400 lg:text-xl lg:font-bold hover:text-white transition text-sm"
         >
           Reset count
         </button>
@@ -103,11 +107,11 @@ function Counter({ selectedbtn }) {
       {/* Right - Motivation */}
       <div className="w-full lg:w-1/5 order-last lg:order-none ">
         <div className="bg-[#0f2d1f] rounded-xl p-4">
-          <p className="text-xs text-gray-400 mb-3">Motivation</p>
-          <p className="text-sm text-gray-300 border-l-2 border-green-600 pl-3 leading-relaxed">
+          <p className="text-xs lg:text-xl text-gray-400 mb-3">Motivation</p>
+          <p className="text-sm lg:text-2xl text-gray-300 border-l-2 border-green-600 pl-3 leading-relaxed">
             "Whoever says SubhanAllah 100 times, a thousand good deeds are
             recorded."
-            <span className="block text-xs text-gray-500 mt-2">
+            <span className="block text-xs lg:text-xl text-gray-500 mt-2">
               — Sahih Muslim
             </span>
           </p>
